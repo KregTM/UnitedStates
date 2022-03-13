04c7369cef289b876c8f2ac7f22eb2c8a1b59b9d
Zahar123456 <71208972+KregTM@users.noreply.github.com>
2022-03-13 15:27:50 +0600

    Initial commit

---
+++ app/src/main/java/com/ubisoft/unitedstates/initFragment/InitialFragment.kt
@@ -0,0 +1,29 @@
+package com.ubisoft.unitedstates.initFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.ubisoft.unitedstates.R
+
+class InitialFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val nextButton = view.findViewById<Button>(R.id.button_1)
+        nextButton.setOnClickListener {
+            println("Hello, World!")
+        }
+    }
+}
